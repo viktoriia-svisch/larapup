a9cbc9d348d29e134eab10dbdbcea5148cd8612f
Viktoriia <viktoriia.svisch@devit-team.com>
2021-04-05 22:48:14 +0000

    update create faculty

---
+++ front-end/src/app/modules/coordinator/create-faculty/create-faculty.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CreateFacultyComponent } from './create-faculty.component';
+describe('CreateFacultyComponent', () => {
+  let component: CreateFacultyComponent;
+  let fixture: ComponentFixture<CreateFacultyComponent>;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateFacultyComponent ]
+    })
+    .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateFacultyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
