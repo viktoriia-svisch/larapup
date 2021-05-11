73d68dccbab1772e4a7c8ed126a17559f49dda45
Viktoriia <viktoriia.svisch@devit-team.com>
2021-05-11 13:36:21 +0000

    finish search student

--- back-end/routes/student.api.php
+++ back-end/routes/student.api.php
@@ -5,5 +5,5 @@ Route::group([
     Route::get('','StudentController@index');
     Route::get('get/{id}','StudentController@show');
     Route::post('new-student','StudentController@store');
-    Route::get('search', 'StudentController@search');
+    Route::get('search/{request}', 'StudentController@search');
 });
