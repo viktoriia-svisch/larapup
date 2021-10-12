e077cff0a4a0484ad8f13784947997cc8cd23aab
Viktoriia <viktoriia.svisch@devit-team.com>
2021-10-12 19:50:19 +0000

    update

--- back-end/app/Http/Controllers/Coordinator/CoordinatorController.php
+++ back-end/app/Http/Controllers/Coordinator/CoordinatorController.php
@@ -1,15 +1,11 @@
 <?php
 namespace App\Http\Controllers\Coordinator;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateFaculty;
 use App\Http\Resources\Coordinator as CoordinatorResource;
-use App\Http\Resources\Faculty as FacultyResource;
 use App\Models\Coordinator;
 use App\Models\Faculty;
-use App\Models\Semester;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Collection;
-use Carbon\Carbon;
 class CoordinatorController extends Controller
 {
     public function index()
@@ -26,39 +22,42 @@ class CoordinatorController extends Controller
     public function storeFaculty(CreateFaculty $request)
     {
         $coor = new Faculty();
-        $coor->semester_id= $request->get('semester_id');
+        $coor->semester_id = $request->get('semester_id');
         $coor->name = $request->get('name');
         $coor->first_deadline = $request->get('first_deadline');
         $coor->second_deadline = $request->get('second_deadline');
-        $validated = $request->validated();
-        $checksemesterId = \DB::table('semesters') 
+        $checksemesterId = \DB::table('semesters')
         ->select('semesters.id')
-        ->where('semesters.id',$coor->semester_id)
-        ->first();
-        $getSemesterDate = \DB::table('semesters') 
-        ->where('id','=',$coor->semester_id)
-        ->first();
-        if(!$checksemesterId) 
+            ->where('semesters.id', $coor->semester_id)
+            ->first();
+        $getSemesterDate = \DB::table('semesters')
+        ->where('id', '=', $coor->semester_id)
+            ->first();
+        if (!$checksemesterId) 
         {
-            return $this->responseMessage('There is no semester with id '.$coor->semester_id. " .Please create one first", true);
+            return $this->responseMessage('There is no semester with id ' . $coor->semester_id . " .Please create one first", true);
         }
-        if($coor->first_deadline < $getSemesterDate->start_date || $coor->first_deadline > $getSemesterDate->end_date) 
-         {
-              return $this->responseMessage('Please enter the date between semester start and end date',true);
-         }
-        if($coor->second_deadline < $getSemesterDate->start_date || $coor->second_deadline > $getSemesterDate->end_date)
-         {
-              return $this->responseMessage('Please enter the date between semester start and end date',true);
-         }
-        else if ($coor->save())
-            {
-                return $this->responseMessage(
-                    'New faculty created successfully',
-                    false,
-                    'success',
-                    $coor,
-                );
-            }
+        if ($coor->first_deadline < $getSemesterDate->start_date || $coor->first_deadline > $getSemesterDate->end_date) 
+        {
+            return $this->responseMessage('Please enter the date between semester start and end date', true);
+        }
+        if ($coor->second_deadline < $getSemesterDate->start_date || $coor->second_deadline > $getSemesterDate->end_date)
+        {
+            return $this->responseMessage('Please enter the date between semester start and end date', true);
+        } else if ($coor->save()) {
+            return $this->responseMessage(
+                'New faculty created successfully',
+                false,
+                'success',
+                $coor
+            );
+        }
+        return $this->responseMessage(
+            'New faculty created unsuccessfully',
+            true,
+            'failed',
+            $coor
+        );
     }
     public function show($id)
     {
--- back-end/app/Http/Requests/CreateFaculty.php
+++ back-end/app/Http/Requests/CreateFaculty.php
@@ -1,41 +1,21 @@
 <?php
-
 namespace App\Http\Requests;
-
 use Illuminate\Foundation\Http\FormRequest;
-
-
 class CreateFaculty extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
-
-
          return [
-             
                'semester_id' => 'required',
                'name' => 'required|min:2|bail',
                'first_deadline' => 'required|date|bail',
                'second_deadline'=>'required|after:first_deadline|date|bail'
-
          ];
     }
-
     public function messages()
     {
          return [
