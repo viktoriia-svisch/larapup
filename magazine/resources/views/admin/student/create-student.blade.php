218fe6ba062512038ccfda41f0e68211ce7241e7
Viktoriia <viktoriia.svisch@devit-team.com>
2021-11-30 01:48:42 +0000

    Update create-student.blade.php

--- magazine/resources/views/admin/student/create-student.blade.php
+++ magazine/resources/views/admin/student/create-student.blade.php
@@ -23,7 +23,7 @@
         <hr>
         <form method="post" class="col-md-6 col-12 m-auto">
             <h3>Login credentials</h3>
-            <div class="input-group mb-3">
+            <div class="input-group input-group-alternative mb-3">
                 <input type="text" class="form-control form-control-alternative" placeholder="Student login email"
                        aria-label="Email's student" name="email" id="email">
             </div>
@@ -31,7 +31,7 @@
                 Student will use this email as their login credential. This email cannot be changed by student nor
                 coordinator, only admin is able to update it.
             </small>
-            <div class="input-group mb-3">
+            <div class="input-group input-group-alternative mb-3">
                 <input type="password" class="form-control form-control-alternative" placeholder="Student password"
                        aria-label="Password's student" name="password" id="password">
             </div>
@@ -41,17 +41,36 @@
             </small>
             <hr>
             <h3>Personal Information</h3>
-            <div class="input-group mb-3">
+            <div class="input-group input-group-alternative mb-3">
                 <input type="text" class="form-control form-control-alternative" placeholder="Student first name"
                        aria-label="Student's first name" name="first_name" id="first_name">
             </div>
-            <div class="input-group mb-3">
+            <div class="input-group input-group-alternative mb-3">
                 <input type="text" class="form-control form-control-alternative" placeholder="Student last name"
                        aria-label="Email's student" name="last_name" id="last_name">
             </div>
             <small class="text-muted mb-4 d-block">
                 Student is able to update their first name and last name in their profile page after logging in.
             </small>
+            <p class="text-muted mt-3 pb-0 mb-1">Gender</p>
+            <div class="row m-0 mb-3">
+                <div class="custom-control custom-radio col-6 d-flex justify-content-center align-items-center">
+                    <input name="custom-radio-2" class="custom-control-input" id="genderMale" type="radio">
+                    <label class="custom-control-label" for="genderMale">Male</label>
+                </div>
+                <div class="custom-control custom-radio col-6 d-flex justify-content-center align-items-center">
+                    <input name="custom-radio-2" class="custom-control-input" id="genderFemale" checked="" type="radio">
+                    <label class="custom-control-label" for="genderFemale">Female</label>
+                </div>
+            </div>
+            <div class="input-group input-group-alternative mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                </div>
+                <input class="form-control datepicker" id="dateOfBirth" name="dateOfBirth" placeholder="Date of Birth" type="text">
+            </div>
+            <hr>
+            <button class="btn btn-block m-0 btn-success">Create</button>
         </form>
     </div>
 @endsection
