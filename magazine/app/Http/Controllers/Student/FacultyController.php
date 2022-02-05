24d3ad5c32cda224a21f2e00b59acc4bc2cff8d5
Viktoriia <viktoriia.svisch@devit-team.com>
2022-02-05 18:11:18 +0000

    Update FacultyController.php

--- magazine/app/Http/Controllers/Student/FacultyController.php
+++ magazine/app/Http/Controllers/Student/FacultyController.php
@@ -2,27 +2,37 @@
 namespace App\Http\Controllers\Student;
 use App\Http\Controllers\Controller;
 use App\Models\Faculty;
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 class FacultyController extends Controller
 {
     public function faculty()
     {
-        $facultyBased = Faculty::with(['faculty_student.student', 'semester'])
+        $facultiesPassed = Faculty::with(['faculty_student.student', 'semester'])
             ->whereHas('faculty_student.student', function ($q) {
                 $q->where('id', Auth::guard(STUDENT_GUARD)->user()->id);
-            });
-        $facultiesPassed = $facultyBased->whereHas('semester', function ($query) {
-                $query->where('end_date', "<=", Carbon::now()->toDateTimeString());
-            })->get();
-        $facultiesFuture = $facultyBased->whereHas('semester', function ($query) {
-                $query->where('start_date', ">", Carbon::now()->toDateTimeString());
-            })->get();
-        $currentFaculty = $facultyBased->whereHas('semester', function ($query) {
-            $query->where('start_date', "<=", Carbon::now()->toDateTimeString())
-                ->where('end_date', ">=", Carbon::now()->toDateTimeString());
-        })->first();
-        dd($facultyBased, $currentFaculty, Carbon::now()->toDateTimeString(), Auth::guard(STUDENT_GUARD)->user()->id);
+            })
+            ->whereHas('semester', function ($query) {
+                $query->whereDate('end_date', "<=", Carbon::now()->toDateTimeString());
+            })
+            ->get();
+        $facultiesFuture = Faculty::with(['faculty_student.student', 'semester'])
+            ->whereHas('faculty_student.student', function ($q) {
+                $q->where('id', Auth::guard(STUDENT_GUARD)->user()->id);
+            })
+            ->whereHas('semester', function ($query) {
+                $query->whereDate('start_date', ">", Carbon::now()->toDateTimeString());
+            })
+            ->get();
+        $currentFaculty = Faculty::with(['faculty_student.student', 'semester'])
+            ->whereHas('faculty_student.student', function ($q) {
+                $q->where('id', Auth::guard(STUDENT_GUARD)->user()->id);
+            })
+            ->whereHas('semester', function ($query) {
+                $query->whereDate('start_date', "<", Carbon::now()->toDateTimeString())
+                    ->whereDate('end_date', ">", Carbon::now()->toDateTimeString());
+            })
+            ->first();
         return view('student.faculty.faculties', [
             'passedFaculties' => $facultiesPassed,
             'futureFaculties' => $facultiesFuture,
