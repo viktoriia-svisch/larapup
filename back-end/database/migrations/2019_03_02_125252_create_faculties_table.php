ed6cb75b80c73bd7d7fa79b9c033daf344c1b655
Viktoriia <viktoriia.svisch@devit-team.com>
2022-05-13 11:29:38 +0000

    fix

--- back-end/database/migrations/2019_03_02_125252_create_faculties_table.php
+++ back-end/database/migrations/2019_03_02_125252_create_faculties_table.php
@@ -1,9 +1,16 @@
 <?php
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 class CreateFacultiesTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('faculties', function (Blueprint $table) {
@@ -16,6 +23,12 @@ class CreateFacultiesTable extends Migration
             $table->softDeletes();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('faculties');
--- magazine/app/Http/Controllers/Admin/AdminController.php
+++ magazine/app/Http/Controllers/Admin/AdminController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\CreateFaculty;
 use App\Models\Semester;
 use App\Models\Faculty;
 use App\Models\Student;
-use App\Models\FacultySemester;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
@@ -19,38 +18,59 @@ class AdminController extends Controller
         $sem = Semester::paginate((PER_PAGE));
         dd($sem);
     }
-    public function store(Requestest $request)
+    public function store(Request $request)
     {
-        $ad = new Semester();
-        $ad->name = $request->get('name');
-        $ad->description = $request->get('description');
-        $ad->start_date = $request->get('start_date');
-        $ad->end_date = $request->get('end_date');
-        $validated = $request->validated();
-        $OneYearLimit = Carbon::parse($ad->start_date)->addYear(); 
-        if ($ad->end_date > $OneYearLimit)
+        $semester = new Semester();
+        $semester->name = $request->get('name');
+        $semester->description = $request->get('description');
+        $semester->start_date = $request->get('start_date');
+        $semester->end_date = $request->get('end_date');
+        $OneYearLimit = Carbon::parse($semester->start_date)->addYear();
+        if ($semester->end_date > $OneYearLimit)
         {
               return $this->responseMessage("The end date must not be more than 1 year from start date", true);
         }
-         if ($ad->save())
+         if ($semester->save())
         {
-            return $this->responseMessage(
-                'New semester created successfully',
-                false,
-                'success',
-                $ad
-            );
+            return view('admin.Semester.create-semester')->with($this->responseBladeMessage('New semester created successfully'));
         }
-        return $this->responseMessage('Create unsuccessfully', true);
-        return view('admin.Semester.create-semester');
+        return back()->with($this->responseBladeMessage('Create unsuccessfully', false));
     }
     public function createFaculty(CreateFaculty $request)
     {
         $coor = new Faculty();
-        $coor->semester_id= $request->get('id');
+        $coor->semester_id= $request->get('semester_id');
         $coor->name = $request->get('name');
+        $coor->first_deadline = $request->get('first_deadline');
+        $coor->second_deadline = $request->get('second_deadline');
         $validated = $request->validated();
-        if ($coor->save())
+        $Sem = Semester::find($coor->semester_id); 
+        $today = Carbon::now()->format('Y-m-d');
+        if($Sem)
+        {
+        $DuplicateFaculty = Faculty::where('semester_id','=', Input::get('semester_id'))
+        ->where('name','=',Input::get('name'))->first(); 
+        $SemStartdate = Carbon::parse($Sem->start_date)->format('Y-m-d'); 
+        $SemEnddate = Carbon::parse($Sem->end_date)->format('Y-m-d');
+        }
+        if(!$Sem) 
+        {
+            return $this->responseMessage('There is no semester with id '.$coor->semester_id. " .Please create one first", true);
+        }
+        if($coor->first_deadline < $SemStartdate || $coor->first_deadline > $SemEnddate
+        ||$coor->second_deadline < $SemStartdate || $coor->second_deadline > $SemEnddate) 
+        {
+            return $this->responseMessage('Please enter the date between semester start and end date',true);
+        }
+        if(!empty($DuplicateFaculty))
+          {
+              return $this->responseMessage('Faculty '.$coor->name. ' of semester id '.$coor->semester_id. ' already exist',true);
+          }
+        if($SemStartdate <= $today || $SemEnddate <= $today)
+        {
+            return $this->responseMessage('cannot make new faculty in existing semester or past semester',true);
+        }
+        else if ($coor->save())
             {
                 return $this->responseMessage(
                     'New faculty created successfully',
@@ -59,10 +79,10 @@ class AdminController extends Controller
                     $coor
                 );
             }
-    }
+        }
     public function searchFaculty($semester, $request)
     {
-        $search = FacultySemester::where('name', 'LIKE', '%' . $request . '%')
+        $search = Faculty::where('name', 'LIKE', '%' . $request . '%')
             ->where('semester_id', 'like', '%' . $semester . '%')
             ->get();
         return response()->json($search);
