3e313c7a1738db2cfcebca7f66ac1939015aa1f6
Viktoriia <viktoriia.svisch@devit-team.com>
2022-08-18 00:05:12 +0000

    Merge branch 'Namdo-front' of https://gitlab.com/do.hoangnam9x/larang-topup into Namdo-front

---
+++ magazine/resources/views/student/faculty/faculty-detail-dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends("student.faculty.faculty-detail")
+@section('title', 'Faculty '.$facultySemester->faculty->name.' - Dashboard')
+@push("custom-css")
+@endpush
+@section('faculty-detail')
+    <div class="col-12">
+        <h2 class="heading-title">Submission</h2>
+        <div class="col">
+            ss
+        </div>
+        <div class="col-auto">
+        </div>
+    </div>
+    <hr>
+@endsection
+@push("custom-js")
+@endpush
