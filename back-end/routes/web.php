3bc739fed8fce75db4ca38de3d7b6e81e527b121
Viktoriia <viktoriia.svisch@devit-team.com>
2022-10-16 00:16:22 +0000

    delete deprecated backend

--- back-end/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-Route::redirect('', 'student/login');
-$this->get('guest/login', 'Guest\Auth\AuthController@showLoginForm')->name('guest.login');
-Route::group([
-    'middleware' => ['auth:'.GUEST_GUARD],             
-], function ($router) {
-});
