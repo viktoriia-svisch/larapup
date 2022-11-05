703430a2500f9665790130e5a8367040162e1162
Viktoriia <viktoriia.svisch@devit-team.com>
2022-11-05 14:06:37 +0000

    Merge branch 'Namdo-front'

--- magazine/app/Helpers/StorageHelper.php
+++ magazine/app/Helpers/StorageHelper.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Helpers;
+use Exception;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 class StorageHelper
@@ -16,39 +19,81 @@ class StorageHelper
         3 => 'profiles',
         4 => 'publishes',
     ];
-    public static function saveArticle($idArticle, $file, &$filePath = null)
+    public static function savePublishFileSubmission($idFacultySemester, $idPublish, UploadedFile $file, &$filePath = null)
     {
         $fileName = $file->getClientOriginalName();
-        $filePath = self::getArticlePath($idArticle);
+        $filePath = self::getArticleFilePath($idFacultySemester, $idPublish);
         self::save($file, $filePath, $fileName);
         return $filePath . $fileName;
     }
-    public static function getArticlePath($id, $path = '')
+    public static function getPublishFilePath($idFacultySemester, $idPublish, $path = '')
     {
-        $folderPath = self::getTypeFolder(self::TYPES['ARTICLE']) . $id . '/';
+        $folderPath = self::getTypeFolder(self::TYPES['ARTICLE']) . 'semester/' . $idFacultySemester . '/publish/' . $idPublish . '/';
         return $folderPath . $path;
     }
-    public static function getArticle($id, $path)
+    public static function deletePublishFile($idFacultySemester, $idPublish, $fileDir)
     {
-        return self::disk()->get(self::getArticlePath($id, $path));
+        $dir = self::getArticleFilePath($idFacultySemester, $idPublish) . $fileDir;
+        return self::disk()->delete($dir);
     }
-    public static function saveComment($idArticle, $file, &$filePath = null)
+    public static function getPublishFile($idFacultySemester, $idPublish, $path)
+    {
+        return self::disk()->get(self::getArticleFilePath($idFacultySemester, $idPublish, $path));
+    }
+    public static function saveArticleFileSubmission($idFacultySemester, $idArticle, UploadedFile $file, &$filePath = null)
     {
         $fileName = $file->getClientOriginalName();
-        $filePath = self::getCommentPath($idArticle);
+        $filePath = self::getArticleFilePath($idFacultySemester, $idArticle);
         self::save($file, $filePath, $fileName);
         return $filePath . $fileName;
     }
-    public static function getCommentPath($id, $path = '')
+    public static function getArticleFilePath($idFacultySemester, $idArticle, $path = '')
     {
-        $folderPath = self::getTypeFolder(self::TYPES['COMMENT']) . $id . '/';
+        $folderPath = self::getTypeFolder(self::TYPES['ARTICLE']) . 'semester/' . $idFacultySemester . '/article/' . $idArticle . '/';
         return $folderPath . $path;
     }
-    public static function getComment($id, $path)
+    public static function deleteArticleFile($idFacultySemester, $idArticle, $fileDir)
     {
-        return self::disk()->get(self::getCommentPath($id, $path));
+        $dir = self::getArticleFilePath($idFacultySemester, $idArticle) . $fileDir;
+        return self::disk()->delete($dir);
     }
-    public static function saveProfile($idArticle, $file, &$filePath = null)
+    public static function getArticleFile($id, $path)
+    {
+        return self::disk()->get(self::getArticleFilePath($id, $path));
+    }
+    public static function saveCommentCoordinator($idCoordinator, $idArticle, UploadedFile $file, &$filePath = null)
+    {
+        $fileName = $file->getClientOriginalName();
+        $filePath = self::getCommentCoordinatorPath($idCoordinator, $idArticle);
+        self::save($file, $filePath, $fileName);
+        return $filePath . $fileName;
+    }
+    public static function getCommentCoordinatorPath($idCoordinator, $idArticle, $path = '')
+    {
+        $folderPath = self::getTypeFolder(self::TYPES['COMMENT']) . 'article/' . $idArticle . '/coordinator/' . $idCoordinator . '/';
+        return $folderPath . $path;
+    }
+    public static function getCommentCoordinator($idCoordinator, $id, $path)
+    {
+        return self::disk()->get(self::getCommentCoordinatorPath($idCoordinator, $id, $path));
+    }
+    public static function saveCommentStudent($idStudent, $idArticle, UploadedFile $file, &$filePath = null)
+    {
+        $fileName = $file->getClientOriginalName();
+        $filePath = self::getCommentStudentPath($idStudent, $idArticle);
+        self::save($file, $filePath, $fileName);
+        return $filePath . $fileName;
+    }
+    public static function getCommentStudentPath($idStudent, $idArticle, $path = '')
+    {
+        $folderPath = self::getTypeFolder(self::TYPES['COMMENT']) . 'article/' . $idArticle . '/student/' . $idStudent . '/';
+        return $folderPath . $path;
+    }
+    public static function getCommentStudent($idStudent, $id, $path)
+    {
+        return self::disk()->get(self::getCommentStudentPath($idStudent, $id, $path));
+    }
+    public static function saveProfile($idArticle, UploadedFile $file, &$filePath = null)
     {
         $fileName = $file->getClientOriginalName();
         $filePath = self::getProfilePath($idArticle);
@@ -63,23 +108,6 @@ class StorageHelper
     public static function getProfile($id, $path)
     {
         return self::disk()->get(self::getProfilePath($id, $path));
-    }
-    private static function disk()
-    {
-        return Storage::disk('local');
-    }
-    private static function getTypeFolder($type)
-    {
-        if (!isset(self::TYPE_NAMES[$type]))
-            throw new \Exception('type is undefined');
-        return 'data/local/' . self::TYPE_NAMES[$type] . '/';
-    }
-    public static function save($file, $path, $fileName = '')
-    {
-        if (!$fileName) {
-            $fileName = $file->getClientOriginalName();
-        }
-        self::disk()->putFileAs($path, $file, $fileName);
     }
     public static function copy($sourcePath, $targetPath)
     {
@@ -100,6 +128,23 @@ class StorageHelper
     public static function urlPath($path)
     {
         return self::disk()->url($path);
+    }
+    private static function disk()
+    {
+        return Storage::disk('local');
+    }
+    private static function getTypeFolder($type)
+    {
+        if (!isset(self::TYPE_NAMES[$type]))
+            throw new Exception('type is undefined');
+        return 'data/local/' . self::TYPE_NAMES[$type] . '/';
+    }
+    public static function save($file, $path, $fileName = '')
+    {
+        if (!$fileName) {
+            $fileName = $file->getClientOriginalName();
+        }
+        self::disk()->putFileAs($path, $file, $fileName);
     }
     public static function mimeType($path)
     {
