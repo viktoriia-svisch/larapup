f2fad91c05110ea03d5b022390f1ce016a3fff4d
Viktoriia <viktoriia.svisch@devit-team.com>
2022-12-18 11:06:57 +0000

    Merge branch 'master' into vu

--- magazine/resources/views/coordinator/Faculty/Articles/faculty-detail-listArticle.blade.php
+++ magazine/resources/views/coordinator/Faculty/Articles/faculty-detail-listArticle.blade.php
@@ -61,9 +61,71 @@
         <hr>
         @foreach($articles as $article)
             <div class="card">
-                {{$article->student_id}}
-                <a href="{{route("coordinator.faculty.article.publish", [$facultySemester->faculty_id, $facultySemester->semester_id, $article->id])}}"
-                   class="btn">Publish this</a>
+                <div class="card-body">
+                    <div class="row m-0 p-0">
+                        <div class="col-auto d-flex justify-content-center align-items-center">
+                            <img src="{{$article->student->avatar_path}}" class="img-center img-fluid rounded-circle"
+                                 style="width: 70px; height: 70px; overflow: hidden;"
+                                 alt="{{$article->student->first_name. " " . $article->student->last_name}}">
+                        </div>
+                        <div class="col row ml-0">
+                            <div class="col">
+                                <h2>{{$article->student->first_name. " " . $article->student->last_name}}</h2>
+                                <hr class="mt-2 mb-0">
+                                <div class="col-12 row m-0 p-0 pb-3">
+                                    <div class="col p-0">
+                                        Submitted at: {{$article->created_at}}
+                                    </div>
+                                    <div class="col p-0">
+                                        Last Update: {{\App\Helpers\DateTimeHelper::formatDateTime($article->updated_at)}}
+                                    </div>
+                                </div>
+                                <div class="col-12 row m-0 p-0">
+                                    @foreach($article->article_file as $file)
+                                        <div class="col-12 col-md-6 p-2">
+                                            <a class="card text-black" href="#">
+                                                <div class="card-body row m-0 p-3">
+                                                    <div class="col-auto d-flex align-items-center">
+                                                        <div
+                                                            class="icon icon-shape bg-default text-white rounded-circle shadow">
+                                                            @if ($file->type == 0)
+                                                                <i class="fas fa-file-word"></i>
+                                                            @else
+                                                                <i class="fas fa-file-image"></i>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                    <div class="col overflow-hidden d-flex align-items-center">
+                                                        <span class="font-weight-bold">
+                                                            {{\Illuminate\Support\Str::limit($file->title, 15)}}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div class="col-auto d-flex flex-column justify-content-around p-0 m-0">
+                                <h2 class="text-center">Grade</h2>
+                                <h1 class="text-center m-0 @if($article->grade > 7) text-success @elseif ($article->grade > 4)
+                                    text-warning @else text-danger @endif">
+                                    {{$article->grade}}
+                                </h1>
+                                <div class="d-flex flex-column justify-content-around p-0 m-0">
+                                    <a href="{{route("coordinator.faculty.article.publish", [$facultySemester->faculty_id, $facultySemester->semester_id, $article->id])}}"
+                                       class="btn btn-primary m-0 mb-1">
+                                        Discuss
+                                    </a>
+                                    <a href="{{route("coordinator.faculty.article.publish", [$facultySemester->faculty_id, $facultySemester->semester_id, $article->id])}}"
+                                       class="btn btn-success mt-1">
+                                        Publish
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         @endforeach
     </div>
