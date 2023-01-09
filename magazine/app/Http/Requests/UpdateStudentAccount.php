f893b1064b937905b8ec077d56346c74b8c46578
Viktoriia <viktoriia.svisch@devit-team.com>
2023-01-09 09:48:30 +0000

    Fix lại validate cho cả student lẫn coordinator

--- magazine/app/Http/Requests/UpdateStudentAccount.php
+++ magazine/app/Http/Requests/UpdateStudentAccount.php
@@ -11,13 +11,19 @@ class UpdateStudentAccount extends FormRequest
     public function rules()
     {
         return [
-            'new_password' =>['same:confirm_password','bail']
+            'new_password' =>['same:confirm_password','bail'],
+            'first_name' => ['same:required|min:2|max:40|bail'],
+            'last_name' => ['same:required|min:2|max:80|bail'],
+            'gender' => 'required|integer',
+            'dateOfBirth' => ['required', 'date_format:d/m/Y']
         ];
     }
     public function messages()
     {
         return [
             'new_password.same' =>  'Confirm Password must be coincided with New Password',
+            'first_nam.same' => 'The length must longer than 2 character',
+            'last_name.same' => 'The length must longer than 2 character',
         ];
     }
 }
